597d638136ca260ecebbaf7061ff0c51ef96273d
cristobalmaier <cristobalmaier1@gmail.com>
2025-05-28 19:31:26 -0300

    avanzamos en la seccion de directivos, en el backend, falta terminarlo

--- src/web/rutas/directivos.rutas.js
+++ src/web/rutas/directivos.rutas.js
@@ -4,11 +4,17 @@ const directivosRutas = new Router()
 // ! Agregar funcion esDirectivo mas tarde
 // import { esDirectivo } from '../utiles/auth.js'
 import { obtenerDatosToken } from '../utiles/obtenerDatosToken.js'
+import {peticion} from "../utiles/peticion.js"
 
 // Ruta para el panel de directivos
-directivosRutas.get('/panel/directivos', (req, res) => {
+directivosRutas.get('/panel/directivos', async (req, res) => {
     const usuario = obtenerDatosToken(req)
-    res.render('paneles/directivos', { titulo: 'AUKA - Panel', usuario })
+    const llamados = await peticion ({url: `${process.env.API_URL}/llamados`, metodo:`GET`}) 
+    const llamadosResultado = await llamados.json()
+
+
+    res.render('paneles/directivos', { titulo: 'AUKA - Panel', usuario,llamadosResultado})
+    console.log(llamadosResultado)
 })
 
 export default directivosRutas
